3ab0d070fbacb0b5dfae29cdc31f7c35c39e60e7
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-02-11 00:15:38 -0600

    Fix setup.py install package_data

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from __future__ import print_function, division, absolute_import
 
-from distutils.core import setup
-from setuptools import find_packages
-
+import os
 import versioneer
+from setuptools import setup, find_packages
+
+datadir = os.path.join('dec2', 'formulas')
+all_directories = [[os.path.join(d, f) for f in folders] for d, folders, files in os.walk(datadir)]
+flattern = [item for sublist in all_directories for item in sublist]
+package_data = [folder[len('dec2/'):] + '/*' for folder in flattern]
 
 setup(name='dec2',
       version=versioneer.get_version(),
@@ -15,6 +19,7 @@ setup(name='dec2',
       license='Apache 2.0',
       packages=find_packages(),
       include_package_data=True,
+      package_data={'dec2': package_data},
       entry_points="""
         [console_scripts]
         dec2=dec2.cli:start
