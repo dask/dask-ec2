4eb7a690dde466055333968b1814ecb47c6178b3
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-02-16 14:57:50 -0600

    Add docker framework for testing

---
+++ dec2/tests/salt/test_distributed.py
@@ -0,0 +1,15 @@
+import pytest
+
+requests = pytest.importorskip("distributed")
+
+from utils import remotetest, cluster, invoke, assert_all_true
+
+
+# def setup_module(module):
+#     utils.invoke('dask-distributed', 'install')
+
+
+@remotetest
+def test_dask(cluster):
+    response = cluster.salt_call("*", "state.sls", ["dask.distributed"])["return"][0]
+    assert_all_true(response)
